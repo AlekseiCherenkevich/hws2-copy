45b2abf5529a2e1d58643bef92c11ab3d8c6e1e7
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2022-12-18 16:28:02 +0300

    fix showing loader

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -17,12 +17,13 @@ const HW10 = () => {
     // useSelector, useDispatch // пишет студент
     const dispatch = useDispatch()
     const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    console.log(isLoading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-            dispatch(loadingAC(false))
+            dispatch(loadingAC(true))
             setTimeout(()=>{
-                dispatch(loadingAC(true))
+                dispatch(loadingAC(false))
             }, 1500)
         // setTimeout
     }
@@ -33,7 +34,7 @@ const HW10 = () => {
             <div className={s2.hwTitle}>Homework #10</div>
 
             <div className={s2.hw}>
-                {!isLoading ? (
+                {isLoading ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
                     </div>
